4b195d34601e55ab9ba62ac04f7cb6bbbf734017
Benjamin Strycker <buzzcutben@hotmail.com>
2024-04-01 20:51:34 -0400

    Final updates.

--- challenge-scripts.js
+++ challenge-scripts.js
@@ -1,3 +1,12 @@
+var today_date= new Date()
+var myyear=today_date.getFullYear()
+var mymonth=today_date.getMonth()+1
+var myToday=today_date.getDate()
+
+
+document.write("Today's date:")
+document.write(mymonth+"/"+myToday+"/"+myyear)
+
 let PokemonInfoList = document.querySelector("ul");
 
 fetch("https://pokeapi.co/api/v2/pokemon?limit=5&offset=0")
@@ -15,12 +24,12 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=5&offset=0")
     }
     })
 
- /*   for (const p of pokemon) {
+   /* for (const p of pokemon) {
         let listItem = document.createElement("ul")
         listItem.innerText = p.name;
         PokemonInfoList.appendChild(listItem);
     } 
-*/
+    */
 
     fetch("https://pokeapi.co/api/v2/type?limit=5&offset=0")
     .then(function (response) {
